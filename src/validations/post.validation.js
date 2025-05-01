297e2bbe6f1b3e537869132155056140cde274c7
Jungho Kim <jeonghogim318@gmail.com>
2025-05-01 14:33:46 +0900

    set: post validtation 작성

--- src/validations/post.validation.js
+++ src/validations/post.validation.js
@@ -1,1 +1,15 @@
+const Joi = require('joi');
 
+//스키마정의
+const postSchema = Joi.object({
+    title: Joi.string().min(3).max(10).required(),
+    content: Joi.string().min(5).max(500).required()
+})
+
+//검증 미들웨어
+const validatePost = (req, res, next) => {
+    postSchema.validate(req.body);
+    next();
+}
+
+module.exports = validatePost;
