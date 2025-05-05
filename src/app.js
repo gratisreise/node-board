dce46a3db90573cf174b5f2f54234aa5c0dcf1eb
Jungho Kim <jeonghogim318@gmail.com>
2025-05-05 14:49:31 +0900

    feat: 게시판 api

--- src/app.js
+++ src/app.js
@@ -1,1 +1,24 @@
 const express = require('express')
+const userRoutes = require('./routes/user.route')
+const postRoutes = require('./routes/post.route')
+const commentRoutes = require('./routes/comment.route')
+const connectDB = require('./config/database');
+const cors = require('cors');
+
+dotenv.config();
+connectDB();
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.use('/api/posts', postRoutes);
+app.use('/api/comments', commentRoutes);
+app.use('/api/users', userRoutes);
+
+app.get('/', (req, res) => {
+    console.log("Welcome to Node.js");
+})
+
+module.exports = app;
